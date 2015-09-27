67f40340e74b2cbf06fe4b38b8babf74ff525eab
Jack Desert <jackdesert@gmail.com>
2015-09-27 22:18:46 +0000

    using IP Address as redis key

--- dick.py
+++ dick.py
@@ -1,8 +1,11 @@
-from flask import Flask
-import requests, json
+from flask import Flask, request
+import requests, json, redis
 app = Flask(__name__)
 
 defs = []
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+redisClient = redis.Redis(connection_pool=pool)
+
 @app.route("/")
 def hello():
     r = requests.get("https://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=test&apikey=bxM09RSxa7VFkARAAYJJ8XSw4XXavRsu")
@@ -17,17 +20,29 @@ def printWord(word):
     contentString = content.decode('utf-8')
     myDict = json.loads(contentString)        # type is 'dict'
     results = myDict['results']
-    print(results)
-    firstResult = results[0]
-    print(firstResult)
-    senses = firstResult['senses']
-    print(senses)
-    firstSense = senses[0]
-    print(firstSense)
-    definition = firstSense['definition'][0]
-    defs.append("%s: %s" % (word, definition))
 
-    return('<br>'.join(reversed(defs)))
+    resultsWithDefinitions = []
+    for result in results:
+        if result['senses']:
+            resultsWithDefinitions.append(result)
+
+    firstResult = resultsWithDefinitions[0]
+    senses = firstResult['senses']
+    firstSense = senses[0]
+    definition = firstSense['definition'][0]
+    definitionWithWord = "%s: %s" % (word, definition)
+    defs.append(definitionWithWord)
+    # Get ip from nginx if available
+
+    ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    redisClient.lpush(ip_address, definitionWithWord)
+
+    defsInRedis = redisClient.lrange(ip_address, 0, 10)
+    toString = lambda x : x.decode('utf-8')
+    defsInRedisStrings = list(map(toString, defsInRedis))
+    #import pdb; pdb.set_trace();
+
+    return('<br>'.join(defsInRedisStrings))
 
 
 if __name__ == "__main__":
