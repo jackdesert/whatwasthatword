9f39d0fa4ead976f8814471f3d7e834601675ebd
Jack Desert <jackdesert@gmail.com>
2015-09-29 01:29:04 +0000

    Returning JSON to front end in the correct format

--- dick.py
+++ dick.py
@@ -12,23 +12,55 @@ def hello():
     return "Basic"
 
 
-@app.route("/<word>")
-def printWord(word):
-    url = "https://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=%s&apikey=bxM09RSxa7VFkARAAYJJ8XSw4XXavRsu" % word
+@app.route("/<wordstring>")
+def printWord(wordstring):
+    url = "https://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=%s&apikey=bxM09RSxa7VFkARAAYJJ8XSw4XXavRsu" % wordstring
     r = requests.get(url)
     content = r.content                       # type is 'bytes'
     contentString = content.decode('utf-8')
     myDict = json.loads(contentString)        # type is 'dict'
     results = myDict['results']
 
-    # Only results that have senses
-    results = list(filter(lambda x: x['senses'], results))
+    word_object = { 'word' : wordstring, 'parts_of_speech' : {} }
 
-    # Keep only where headword matches exactly
-    results = list(filter(lambda x: x['headword'].lower() == word.lower(), results))
+    for result in results:
+        if not result['senses']:
+            # Some results have no senses
+            continue
+        if not result['headword'].lower() == wordstring.lower():
+            # Some results are for compound words that include the headword
+            continue
+        if not 'part_of_speech' in result:
+            # Some results do not name a 'part_of_speech' key
+            continue
 
-    # Keep only where part_of_speech is given
-    results = list(filter(lambda x: 'part_of_speech' in x, results))
+        part_of_speech = result['part_of_speech']
+
+
+        headword = result['headword']
+
+        #import pdb; pdb.set_trace();
+
+        for sense in result['senses']:
+            if not 'definition' in sense: continue
+            # Note we only look for first definition
+            definition_object = { 'definition' : sense['definition'][0] }
+            if 'examples' in sense:
+                definition_object['example'] = sense['examples'][0]['text']
+
+            word_object['parts_of_speech'].setdefault(part_of_speech, []).append(definition_object)
+
+        if 'pronunciations' in result:
+            mp3 = "https://api.pearson.com%s" % result['pronunciations'][0]['audio'][0]['url']
+            ipa = result['pronunciations'][0]['ipa']
+            word_object['pronunciation'] = { 'ipa' : ipa, 'mp3' : mp3 }
+
+    word_object_json = json.dumps(word_object)
+
+
+
+
+
 
     #for result in results:
     #    if result['senses']:
@@ -36,30 +68,17 @@ def printWord(word):
 
     # Get ip from nginx if available
     ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-
-    for result in results:
-        part_of_speech = result['part_of_speech']
-        headword = result['headword']
-        for sense in result['senses']:
-            if not 'definition' in sense: continue
-            #import pdb; pdb.set_trace();
-            for definition in sense['definition']:
-                definitionWithWord = "%s (%s): %s" % (headword, part_of_speech, definition)
-                redisClient.lpush(ip_address, definitionWithWord)
-
-    #firstResult = results[0]
-    #senses = firstResult['senses']
-    #firstSense = senses[0]
-    #definition = firstSense['definition'][0]
-    #definitionWithWord = "%s: %s" % (word, definition)
-    #defs.append(definitionWithWord)
+    redisClient.lpush(ip_address, word_object_json)
 
 
-    defsInRedis = redisClient.lrange(ip_address, 0, 10)
+
+
+    words_in_redis = redisClient.lrange(ip_address, 0, 10)
     toString = lambda x : x.decode('utf-8')
-    defsInRedisStrings = list(map(toString, defsInRedis))
+    words_in_redis_strings = list(map(toString, words_in_redis))
 
-    return('<br>'.join(defsInRedisStrings))
+    return('<hr>'.join(words_in_redis_strings))
+    #return(json.dumps(word_object))
 
 
 if __name__ == "__main__":
