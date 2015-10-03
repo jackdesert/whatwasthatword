6ed79ae964aeb3044c91c56fd6d1b466bc805be5
Jack Desert <jackdesert@gmail.com>
2015-10-03 22:30:48 +0000

    disregard numbers in ids

--- word.py
+++ word.py
@@ -1,29 +1,34 @@
-import requests, json
+import requests
 from lxml import etree
+import re
+import pdb
 
 class Word:
     URL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/%s?key=5df5392d-be97-4dc9-b79d-889371de0aa4"
     def __init__(self, headword):
-        self.headword = headword
-        self.data = { 'word' : headword, 'parts_of_speech' : {} }
-    def json(self):
+        self._headword = headword
+        self._data = { 'word' : headword, 'parts_of_speech' : {}, 'pronunciation': {'ipa': 'blah', 'mp3': 'mp3.com' }}
+    def data(self):
         self._run()
-        return json.dumps(self.data)
+        return self._data
     def _run(self):
         self._fetch()
         self._remove_extraneous_entries()
         self._process_entries()
     def _fetch(self):
-        url = self.URL % self.headword
-        #print('requesting %s' % self.headword)
+        url = self.URL % self._headword
+        #print('requesting %s' % self._headword)
         r = requests.get(url)
-        #print('completed  %s' % self.headword)
+        #print('completed  %s' % self._headword)
         content = r.content                       # type is 'bytes'
         self.root = etree.fromstring(content)
     def _remove_extraneous_entries(self):
         self.entries = []
         for entry in self.root:
-            if entry.get('id') == self.headword:
+            id = entry.get('id')
+            # convert "run[2]" to "run"
+            id_plain = re.sub('[^a-zA-Z]', '', id)
+            if id_plain == self._headword:
                 self.entries.append(entry)
     def _process_entries(self):
         for entry in self.entries:
@@ -57,4 +62,4 @@ class Word:
                 dt_data['synonyms'] = synonyms
             definitions.append(dt_data)
 
-        self.data['parts_of_speech'][part_of_speech] = definitions
+        self._data['parts_of_speech'][part_of_speech] = definitions
